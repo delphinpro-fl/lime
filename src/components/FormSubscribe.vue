4bdf0771f0c56b96f86be1770ba6e0a895e3a8f0
delphinpro <delphinpro@yandex.ru>
2019-09-10 17:17:32 +0400

    Форма подписки на рассылку

---
+++ src/components/FormSubscribe.vue
@@ -0,0 +1,35 @@
+<script>/*!
+ * Lime project
+ * File: FormSubscribe.vue
+ * (c) 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export default {
+    name: 'FormSubscribe',
+
+    computed: {
+        uid() {
+            return 'id-form-subscribe-' + this._uid;
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="form-subscribe">
+        <form action="">
+            <div class="form-subscribe__container">
+                <label :for="uid" class="form-subscribe__label">Подпишитесь на рассылку</label>
+                <input class="form-subscribe__input"
+                    type="email"
+                    name="email"
+                    :id="uid"
+                    placeholder="Введите ваш e-mail"
+                >
+            </div>
+        </form>
+    </div>
+</template>
+
+<style lang="scss" src="../styles/components/FormSubscribe.scss"></style>

1c5be12cce538b56b0823808f06512dace3655be
delphinpro <delphinpro@yandex.ru>
2019-09-12 12:04:20 +0400

    Добавлен компонент Checkbox.vue

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,36 @@
+<!--
+  Lime project
+  File: Checkbox.vue
+  (c) 2019 delphinpro <delphinpro@gmail.com>
+  licensed under the MIT license
+-->
+
+<script>
+export default {
+    name: 'Checkbox',
+
+    model: {
+        prop : 'checked',
+        event: 'change',
+    },
+
+    props: {
+        value  : [String, Number, Boolean],
+        checked: { type: Boolean, default: false },
+    },
+};
+</script>
+
+<template>
+    <div class="checkbox">
+        <label class="checkbox__label">
+            <input class="checkbox__element" type="checkbox" :checked="checked" @change="$emit('change', $event.target.checked)">
+            <span class="checkbox__indicator"></span>
+            <span class="checkbox__text">
+                <slot></slot>
+            </span>
+        </label>
+    </div>
+</template>
+
+<style lang="scss" src="../styles/components/Checkbox.scss"></style>

63de953d2abdf43272cfae0d8a5ddd716be673b8
delphinpro <delphinpro@yandex.ru>
2019-09-13 14:52:02 +0400

    Иконки для навигации слайдера

---
+++ src/components/Icons/IconAngleLeft.vue
@@ -0,0 +1,16 @@
+<!--
+  Lime project
+  File: IconAngleLeft.vue
+  (c) 2019 delphinpro <delphinpro@gmail.com>
+  licensed under the MIT license
+-->
+
+<script>
+export default { name: 'IconAngleLeft' };
+</script>
+
+<template>
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38 76">
+        <path fill="none" stroke="currentColor" stroke-width="4" d="M35,2L3,37.7L35,74"/>
+    </svg>
+</template>

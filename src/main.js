c393582b208158c89e5eb4f0a91f5eee01b01a45
delphinpro <delphinpro@yandex.ru>
2019-09-15 14:49:33 +0400

    Установка и подключение vue2-touch-events

--- src/main.js
+++ src/main.js
@@ -7,13 +7,20 @@
 
 import 'normalize.css/normalize.css';
 import '@/styles/main.scss';
-import Vue    from 'vue';
-import App    from './App.vue';
-import router from './router';
-import store  from './store';
+import Vue             from 'vue';
+import Vue2TouchEvents from 'vue2-touch-events';
+import App             from './App.vue';
+import router          from './router';
+import store           from './store';
 
 
 Vue.config.productionTip = false;
+Vue.use(Vue2TouchEvents, {
+    touchClass         : '',
+    tapTolerance       : 10,
+    swipeTolerance     : 80,
+    longTapTimeInterval: 400,
+});
 
 new Vue({
     router,

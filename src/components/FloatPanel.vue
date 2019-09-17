423b0eb53e9ef9359ceb0200af62bed12598da7b
delphinpro <delphinpro@yandex.ru>
2019-09-17 11:27:02 +0400

    Компонент FloatPanel.vue, панель входа/реги/лк/etc.

---
+++ src/components/FloatPanel.vue
@@ -0,0 +1,39 @@
+<!--
+  Lime project
+  File: FloatPanel.vue
+  (c) 2019 delphinpro <delphinpro@gmail.com>
+  licensed under the MIT license
+-->
+
+<script>
+import { mapState } from 'vuex';
+
+
+export default {
+    name: 'FloatPanel',
+
+    computed: {
+        ...mapState({
+            hashNav: state => state.hashNav,
+        }),
+
+        isActive() {
+            return this.hashNav !== '';
+        },
+    },
+
+    methods: {
+        selfClose() {
+            this.$router.back();
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="float-panel" v-if="isActive">
+        <div class="float-panel__close" @click="selfClose">&times;</div>
+    </div>
+</template>
+
+<style lang="scss" src="../styles/components/FloatPanel.scss"></style>

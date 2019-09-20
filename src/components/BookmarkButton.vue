ccefa3fe3dc21953a9801b2690555618b336e6fa
delphinpro <delphinpro@yandex.ru>
2019-09-20 15:46:35 +0400

    Кнопка-звездочка "Закладки"

---
+++ src/components/BookmarkButton.vue
@@ -0,0 +1,44 @@
+<!--
+  Lime project
+  File: BookmarkButton.vue
+  (c) 2019 delphinpro <delphinpro@gmail.com>
+  licensed under the MIT license
+-->
+
+<script>
+import IconStar from '@/components/Icons/IconStar';
+
+
+export default {
+    name: 'BookmarkButton',
+
+    components: {
+        IconStar,
+    },
+
+    props: {
+        active: { type: Boolean, default: false },
+        hover : { type: Boolean, default: false },
+    },
+
+    computed: {
+        computedClasses() {
+            return {
+                BookmarkButton_active: this.active,
+                BookmarkButton_hover : this.hover,
+            };
+        },
+    },
+};
+</script>
+
+<template>
+    <button class="BookmarkButton"
+        :class="computedClasses"
+        @click="$emit('click', $event)"
+    >
+        <IconStar/>
+    </button>
+</template>
+
+<style lang="scss" src="../styles/components/BookmarkButton.scss"></style>

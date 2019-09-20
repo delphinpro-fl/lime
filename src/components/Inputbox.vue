2f7ce7c08bd4470f901a03efb5fc2111679d2788
delphinpro <delphinpro@yandex.ru>
2019-09-20 15:58:40 +0400

    Поле ввода Inputbox

---
+++ src/components/Inputbox.vue
@@ -0,0 +1,30 @@
+<!--
+  Lime project
+  File: Inputbox.vue
+  (c) 2019 delphinpro <delphinpro@gmail.com>
+  licensed under the MIT license
+-->
+
+<script>
+export default {
+    name: 'Inputbox',
+
+    props: {
+        prompt: String,
+        label : String,
+        value : [String, Number],
+    },
+};
+</script>
+
+<template>
+    <div class="inputbox">
+        <div class="inputbox__header">
+            <span class="inputbox__prompt">{{prompt}}</span>
+            <span class="inputbox__label">{{label}}</span>
+        </div>
+        <input type="text" class="inputbox__input" :value="value" @input="this.$emit('input', $event)">
+    </div>
+</template>
+
+<style lang="scss" src="../styles/components/Inputbox.scss"></style>

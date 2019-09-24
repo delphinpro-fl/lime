20384e8a8e3272d906f9a67e55afaabfaf66e73c
delphinpro <delphinpro@yandex.ru>
2019-09-24 21:35:24 +0400

    Add: Компонент отображения в попапе наличия товара в магазинах

---
+++ src/components/Availability.vue
@@ -0,0 +1,148 @@
+<!--
+  Lime project
+  File: Availability.vue
+  (c) 2019 delphinpro <delphinpro@gmail.com>
+  licensed under the MIT license
+-->
+
+<script>
+import { mapGetters }      from 'vuex';
+import LoadingIndicator    from '@/components/LoadingIndicator';
+import DropdownList        from '@/components/DropdownList';
+import { api }             from '@/lib/api';
+import { telephoneAsLink } from '@/lib';
+
+
+export default {
+    name: 'Availability',
+
+    components: {
+        LoadingIndicator,
+        DropdownList,
+    },
+
+    data: () => ({
+        isListView: true,
+
+        cities     : [],
+        citiesReady: false,
+        cityIndex  : null,
+
+        shops     : [],
+        shopsReady: false,
+    }),
+
+    computed: {
+        ...mapGetters([
+            'currentSKU',
+        ]),
+
+        selectedCity() {
+            if (this.cityIndex in this.cities) return this.cities[this.cityIndex];
+            return null;
+        },
+
+        shopList() {
+            return this.shops.map(shop => {
+                let s = this.currentSKU.rests.find(item => item.shop === shop.id);
+                return {
+                    ...shop,
+                    sizes: s ? s.sizes : [],
+                };
+            });
+        },
+
+        optionsForCities() {
+            return this.cities.map(item => {
+                return {
+                    title: item.name,
+                    value: item.id,
+                };
+            });
+        },
+    },
+
+    async mounted() {
+        this.citiesReady = false;
+        let response     = await api.getCityList({});
+        this.cities      = response.items;
+        if (this.cities.length) this.cityIndex = 0;
+        this.citiesReady = true;
+
+        if (this.selectedCity) {
+            this.updateShopList(this.selectedCity.id);
+        } else {
+            this.shopsReady = true;
+        }
+    },
+
+    methods: {
+        pickCity(cityIndex) {
+            this.cityIndex = cityIndex;
+            console.log('pick');
+            this.updateShopList(this.selectedCity.id);
+        },
+
+        async updateShopList(cityId) {
+            this.shopsReady = false;
+            let response    = await api.getShopList({ cityId });
+            this.shops      = response.items;
+            this.shopsReady = true;
+        },
+
+        phoneLink(phone) {
+            return telephoneAsLink(phone);
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="Availability" v-if="citiesReady">
+        <h1>Наличие в магазинах</h1>
+        <p><strong>Этот товар вы сможете приобрести в магазинах LIMÉ:</strong></p>
+        <div class="Availability__city-selector" v-if="cities.length">
+            <p><strong>Выберите город</strong></p>
+            <DropdownList
+                placeholder="Город не выбран"
+                :selected="cityIndex"
+                :options="optionsForCities"
+                @change="pickCity"
+            />
+        </div>
+
+        <template v-if="shopsReady">
+            <ul class="Availability__switch-links switch-links">
+                <li class="switch-links__item" :class="{active:isListView}" @click="isListView=true">Список</li>
+                <li class="switch-links__delimiter">|</li>
+                <li class="switch-links__item" :class="{active:!isListView}" @click="isListView=false">Карта</li>
+            </ul>
+
+            <div class="Availability__shops" v-if="shopsReady">
+                <div class="shop-list" v-if="isListView">
+                    <div class="shop-list__item" v-for="item in shopList">
+                        <div class="shop-list__row"><strong>{{item.name}}</strong></div>
+                        <div class="shop-list__row">
+                            <a :href="phoneLink(item.phone)">{{item.phone}}</a>
+                        </div>
+                        <div class="shop-list__row">{{item.address}}</div>
+                        <div class="shop-list__row" v-if="item.sizes&&item.sizes.length">{{item.sizes.join(', ')}}</div>
+                    </div>
+                </div>
+                <div v-else>
+                    <h3 style="color:red">Here will be a map. Later...</h3>
+                </div>
+            </div>
+
+            <div class="Availability__note">
+                Информация носит справочный характер.
+                Мы рекомендуем Вам позвонить в магазин, чтобы проверить наличие и
+                зарезервировать модель.
+            </div>
+        </template>
+        <LoadingIndicator class="Availability__indicator" v-else/>
+    </div>
+    <LoadingIndicator v-else/>
+</template>
+
+<style lang="scss" src="../styles/components/Availability.scss"></style>

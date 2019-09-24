dde2546f400ef152d64ed9fdb9b53d5eaf746084
delphinpro <delphinpro@yandex.ru>
2019-09-24 21:20:42 +0400

    Refactor: Селектор размера

--- src/components/SizeSelector.vue
+++ src/components/SizeSelector.vue
@@ -6,8 +6,15 @@
 -->
 
 <script>
+import IconDropdown from '@/components/Icons/IconDropdown';
+
+
 export default {
     name: 'SizeSelector',
+
+    components: {
+        IconDropdown,
+    },
 
     props: {
         options : { type: Array, default: null },
@@ -47,9 +54,7 @@ export default {
         >
             <div class="SizeSelector__selected">{{ selectedText }}</div>
             <div class="SizeSelector__arrow">
-                <svg width="6" height="4" viewBox="0 0 6 4" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M5.10355 1.35355L5.45711 1L4.75 0.292893L4.39645 0.646447L5.10355 1.35355ZM2.75 3L2.37371 3.32925L2.72564 3.73146L3.10355 3.35355L2.75 3ZM1.37629 0.670748L1.04704 0.294459L0.294459 0.952964L0.623712 1.32925L1.37629 0.670748ZM4.39645 0.646447L2.39645 2.64645L3.10355 3.35355L5.10355 1.35355L4.39645 0.646447ZM3.12629 2.67075L1.37629 0.670748L0.623712 1.32925L2.37371 3.32925L3.12629 2.67075Z"/>
-                </svg>
+                <IconDropdown/>
             </div>
         </div>
         <div class="SizeSelector__pane" v-if="isOpen">

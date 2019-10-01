54182473b0310352e5462943ab411c0765b12239
delphinpro <delphinpro@yandex.ru>
2019-10-01 19:12:50 +0400

    Форма восстановления пароля, сообщение успеха

--- src/components/FormRecoveryPassword.vue
+++ src/components/FormRecoveryPassword.vue
@@ -6,42 +6,54 @@
 -->
 
 <script>
-import Inputbox from '@/components/Inputbox';
+import Inputbox         from '@/components/Inputbox';
+import LoadingIndicator from '@/components/LoadingIndicator';
 
 
 export default {
     name: 'FormRecoveryPassword',
 
-    components: { Inputbox },
+    components: { LoadingIndicator, Inputbox },
 
     data: () => ({
-        form: {
+        alreadySent: false,
+        busy       : false,
+        form       : {
             email: null,
         },
     }),
 
     computed: {
         disabledSubmit() {
-            return !this.form.email;
+            return !this.form.email || this.busy;
         },
     },
 
     methods: {
         onSubmit() {
+            this.busy = true;
+            setTimeout(() => {
+                this.busy        = false;
+                this.alreadySent = true;
+            }, 1000);
+        },
 
+        complete() {
+            this.$store.dispatch('closeModal');
         },
     },
 };
 </script>
 
 <template>
-    <form @submit.prevent="onSubmit">
+    <form @submit.prevent="onSubmit" v-if="!alreadySent">
         <div class="form-group">
             <Inputbox
                 type="email"
                 autocomplete="current-password"
                 prompt="Восстановление пароля"
                 label="Введите E-mail"
+                :disabled="busy"
                 v-model="form.email"
             />
         </div>
@@ -55,7 +67,19 @@ export default {
                 :disabled="disabledSubmit"
             >Отправить</button>
         </div>
+        <LoadingIndicator v-if="busy"/>
     </form>
+    <div v-else>
+        <p><strong>Мы получили Ваш запрос</strong></p>
+        <p>В ближайшее время на вашу почту придет сообщение с инструкциями по завершению процесса</p>
+        <div class="form-group form-group-submit">
+            <button
+                class="btn btn-block"
+                type="button"
+                @click="complete"
+            >Ok</button>
+        </div>
+    </div>
 </template>
 
 <style lang="scss"></style>
